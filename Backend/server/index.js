413a1042c141aca2685e9ae4383d0255d66d503f
Omar Ismail <71655271+OmarIsmail7980@users.noreply.github.com>
2021-11-16 15:51:12 -0600

    Delete Backend directory

--- Backend/server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const fs = require("fs");
-const app = express();
-const PORT = 8080;
-
-
-app.listen(PORT, ()=>{
-    console.log("listening on port %d",PORT)
-})
